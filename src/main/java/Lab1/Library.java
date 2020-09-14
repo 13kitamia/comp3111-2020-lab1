b3cbc57d7d527b987f7f62244eb254be2963678d
Anri Kitami <anrikitamilee@gmail.com>
2020-09-14 14:29:45 +0800

    Initial project setup

---
+++ src/main/java/Lab1/Library.java
@@ -0,0 +1,12 @@
+/*
+ * This Java source file was generated using Gradle 'init' task.
+*/
+public class Library {
+	public static void main(String arg[]) {
+		System.out.println("Where there is fire, commit and push.");
+	}
+	
+    public boolean someLibraryMethod() {
+        return true;
+    }
+}
